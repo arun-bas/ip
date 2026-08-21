5b07df5aa49eb71f868b8c952a981f61bebc2516
Arun <arunbaskaran002@gmail.com>
2026-08-21 15:00:56 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
+import javax.swing.tree.ExpandVetoException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int listCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello! I'm Bubba.");
         System.out.println("What can I do for you?");
@@ -17,42 +18,57 @@ public class Duke {
                     break;
                 } else if (input.equals("list")) {
                     System.out.println("Current list of tasks:");
-                    for (int i = 0; i < listCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
                     }
                 } else if (input.startsWith("mark ")) {
                     int taskNumber = Integer.parseInt(input.substring(5));
-                    tasks[taskNumber - 1].done();
+                    tasks.get(taskNumber - 1).done();
                     System.out.println("Good job, this task is done!");
-                    System.out.println(tasks[taskNumber - 1]);
+                    System.out.println(tasks.get(taskNumber - 1));
                 } else if (input.startsWith("unmark ")) {
                     int taskNumber = Integer.parseInt(input.substring(7));
-                    tasks[taskNumber - 1].undoDone();
+                    tasks.get(taskNumber - 1).undoDone();
                     System.out.println("This task has been unmarked.");
-                    System.out.println(tasks[taskNumber - 1]);
-                } else if (input.equals("todo")){
+                    System.out.println(tasks.get(taskNumber - 1));
+                } else if (input.startsWith("delete ")) {
+                    try {
+                        int taskNumber = Integer.parseInt(input.substring(7));
+                        int idx = taskNumber-1;
+                        if (idx<0 || idx >= tasks.size()) {
+                            throw new BubbaException("Task does not exist.");
+                        }
+
+                        Task deletedTask = tasks.remove(idx);
+                        System.out.println("Removed the following task: ");
+                        System.out.println(deletedTask);
+                        System.out.println("Number of tasks in list: " + tasks.size());
+                    } catch (NumberFormatException e) {
+                        throw new BubbaException("Please enter valid task number.");
+                    }
+                }else if (input.equals("todo")){
                     throw new BubbaException("Todo description cannot be empty!");
                 }else if (input.startsWith("todo ")) {
                     String description = input.substring(5);
                     if (description.isEmpty()) {
                         throw new BubbaException("Todo description cannot be empty!");
                     }
-                    tasks[listCount] = new Todo(description);
-                    listCount++;
+                    Task task = new Todo(description);
+                    tasks.add(task);
                     System.out.println("Added new task:");
-                    System.out.println(tasks[listCount - 1]);
-                    System.out.println("Number of tasks in list: " + listCount);
+                    System.out.println(task);
+                    System.out.println("Number of tasks in list: " + tasks.size());
                 } else if (input.startsWith("deadline ")) {
                     String remaining = input.substring(9);
                     String[] parts = remaining.split(" /by ", 2);
                     String description = parts[0];
                     String by = parts[1];
 
-                    tasks[listCount] = new Deadline(description, by);
-                    listCount++;
+                    Task task = new Deadline(description, by);
+                    tasks.add(task);
                     System.out.println("Added new task:");
-                    System.out.println(tasks[listCount - 1]);
-                    System.out.println("Number of tasks in list: " + listCount);
+                    System.out.println(task);
+                    System.out.println("Number of tasks in list: " + tasks.size());
                 } else if (input.startsWith("event ")) {
                     String remaining = input.substring(6);
                     String[] parts = remaining.split(" /from ", 2);
@@ -62,11 +78,11 @@ public class Duke {
                     String from = times[0];
                     String to = times[1];
 
-                    tasks[listCount] = new Event(description, from, to);
-                    listCount++;
+                    Task task = new Event(description, from, to);
+                    tasks.add(task);
                     System.out.println("Added new task: ");
-                    System.out.println(tasks[listCount - 1]);
-                    System.out.println("Number of tasks in list: " + listCount);
+                    System.out.println(task);
+                    System.out.println("Number of tasks in list: " + tasks.size());
                 } else {
                     throw new BubbaException("Sorry, unsure what you mean by that.");
                 }
