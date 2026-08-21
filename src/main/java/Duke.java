d2618e21fc4204166e73e93a9ba62bb940e968ff
Arun <arunbaskaran002@gmail.com>
2026-08-21 13:46:42 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        /*String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Bubba.");
         System.out.println("What can I do for you?");
+        // loop and echo user input until bye.
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+
         System.out.println("Goodbye. See you again.");
+        scanner.close();
     }
 }
